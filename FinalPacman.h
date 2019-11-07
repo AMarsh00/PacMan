3121b794058285256d84de3604b3333a10bb5e07
AMarsh00 <56977848+AMarsh00@users.noreply.github.com>
2019-11-06 20:03:40 -0700

    Added the main header file
    This is a basic file for #includes mainly

---
+++ FinalPacman.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "resource.h"
+#define OBJ_SZ 25
+#define SW 750
+#define SH 502
+#define PS 5
+#include <Windows.h>
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
